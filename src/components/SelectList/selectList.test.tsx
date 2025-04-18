1ff1a0d1e9bb6d894ff638fe71f81368c4de9959
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-18 20:11:51 -0300

    Mocks - GetByText

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react-native"
+import { SelectList } from "@components/SelectList"
+
+describe("Component: SelectList", () => {
+  it("shoud be return city details selected", async() => {
+    const data = [ 
+      { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+      { id: '2', name: 'Campo grande', latitude: 123, longitude: 456 }
+    ]
+
+    render(
+      <SelectList 
+        data={data}
+        onChange={() => {}}
+        onPress={() => {}}
+      />
+    )
+
+    const selectedCity = screen.getByText('Campinas')
+    console.log(selectedCity)
+  })
+})
