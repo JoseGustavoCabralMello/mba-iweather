ec34bc0063a91fef40fb4378a6f9a42df82d292b
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-19 16:57:49 -0300

    Testando Hooks - CustomRender

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react-native"
+import { render, screen, waitFor } from "@__tests__/utils/customRender"
 import { Routes } from "."
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
 
 describe("Routes", () => {
   it('should be render Search screen when not city selecte', async () => {
@@ -10,5 +11,19 @@ describe("Routes", () => {
     const title = await waitFor(() => screen.findByText(/^escolha um local/i))
 
     expect(title).toBeTruthy()
+  })
+
+  it('shoul be render Dashboard screen when has city selected', async () => {
+    const city = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+
+    const { debug } = render(<Routes />)
+     debug()
   })
 })
