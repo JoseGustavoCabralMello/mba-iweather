c548fe0aee45d3910751a134e75a396f065a31c6
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-19 16:27:45 -0300

    Testando Hooks - waitFor e act

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -1,11 +1,12 @@
-import { renderHook } from "@testing-library/react-native"
+import { act, renderHook, waitFor } from "@testing-library/react-native"
 
 import { useCity } from "@hooks/useCity"
-import { CityProvider } from "./CityContext"
+import { CityProvider } from "@contexts/CityContext"
 
 describe('Context: cityContext', () => {
-  it('should be change selected city',  () => {
+  it('should be change selected city', async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
-    console.log(result.current.city)
+    
+    await waitFor(() => act(() => console.log(result.current.city)))
   })
 })
